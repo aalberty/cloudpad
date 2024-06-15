2da2b371ae513f502b512c465cf607c6159598e4
aalberty <46965141+aalberty@users.noreply.github.com>
2024-06-15 10:59:21 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/cloudpad/',  // Replace 'your-repo-name' with your actual repository name
+  base: '/',  // Replace 'your-repo-name' with your actual repository name
 
 });
