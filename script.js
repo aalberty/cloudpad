eac37f699cc3aad21af21b9d90b0775680e9df2d
aalberty <46965141+aalberty@users.noreply.github.com>
2024-06-25 18:10:25 +0000

    .

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
-import { initializeApp } from "firebase/app";
-import { getFirestore, collection, doc, getDoc, setDoc } from "firebase/firestore";
+// import { initializeApp } from "firebase/app";
+// import { getFirestore, collection, doc, getDoc, setDoc } from "firebase/firestore";
 
-let serviceAccount = {
-    "type": "service_account",
-    "projectId": "personal-notepad-94a50",
-    "privateKeyId": "e2c2e8bcf7fd060be55b02ed21d9ed2c404ea6b9",
-    "privateKey": import.meta.env.VITE_FIREBASE_PRIVATE_KEY,
-    "clientEmail": "firebase-adminsdk-3tciv@personal-notepad-94a50.iam.gserviceaccount.com",
-    "clientId": "104199657300634952718",
-    "authUri": "https://accounts.google.com/o/oauth2/auth",
-    "tokenUri": "https://oauth2.googleapis.com/token",
-    "authProviderX509CertUrl": "https://www.googleapis.com/oauth2/v1/certs",
-    "clientX509CertUrl": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-3tciv%40personal-notepad-94a50.iam.gserviceaccount.com",
-    "universeDomain": "googleapis.com"
-};
+// let serviceAccount = {
+//     "type": "service_account",
+//     "projectId": "personal-notepad-94a50",
+//     "privateKeyId": "e2c2e8bcf7fd060be55b02ed21d9ed2c404ea6b9",
+//     "privateKey": import.meta.env.VITE_FIREBASE_PRIVATE_KEY,
+//     "clientEmail": "firebase-adminsdk-3tciv@personal-notepad-94a50.iam.gserviceaccount.com",
+//     "clientId": "104199657300634952718",
+//     "authUri": "https://accounts.google.com/o/oauth2/auth",
+//     "tokenUri": "https://oauth2.googleapis.com/token",
+//     "authProviderX509CertUrl": "https://www.googleapis.com/oauth2/v1/certs",
+//     "clientX509CertUrl": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-3tciv%40personal-notepad-94a50.iam.gserviceaccount.com",
+//     "universeDomain": "googleapis.com"
+// };
 
-const app = initializeApp(serviceAccount);
-const db = getFirestore(app);
+// const app = initializeApp(serviceAccount);
+// const db = getFirestore(app);
 
 document.addEventListener('DOMContentLoaded', function () {
     const textarea = document.getElementById('markdown');
