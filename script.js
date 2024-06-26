10445ba81f3481f32c723b55287ce89e59d15d15
aalberty <46965141+aalberty@users.noreply.github.com>
2024-06-26 19:45:37 +0000

    swapping to JS lib instead of Node.js

--- script.js
+++ script.js
@@ -1,84 +1,175 @@
-
-import { GoogleApis } from "googleapis";
-
+/*
 const SCOPES = ['https://www.googleapis.com/auth/drive.readonly'];
 const CREDS = {
-    id:             import.meta.env.VITE_CLIENT_ID,
-    secret:         import.meta.env.VITE_CLIENT_SECRET,
-    refresh_token:  import.meta.env.VITE_REFRESH_TOKEN,
-    redirect_uri:   "https://alberty.dev"
+  id: import.meta.env.VITE_CLIENT_ID,
+  secret: import.meta.env.VITE_CLIENT_SECRET,
+  refresh_token: import.meta.env.VITE_REFRESH_TOKEN,
+  redirect_uri: "https://alberty.dev"
 };
 
-const google = new GoogleApis();
-const {OAuth2} = google.auth;
 
-const oAuth2Client = new OAuth2(
-    CREDS.id, CREDS.secret, CREDS.redirect_uri
-);
 
-console.log('my client: ', oAuth2Client);
-
-let token = {
-    access_token:   "",
-    refresh_token:  CREDS.refresh_token,
-    expiry_date:    -1,
-    token_type:     "Bearer"
-};
-
-oAuth2Client.setCredentials(token);
-
-// List files in a shared drive
-function listFiles(auth) {
-    const drive = google.drive({ version: 'v3', auth });
-    drive.files.list({
-      pageSize: 10,
-      fields: 'nextPageToken, files(id, name)',
-      supportsAllDrives: true,
-      includeItemsFromAllDrives: true,
-    }, (err, res) => {
-      if (err) return console.error('The API returned an error:', err);
-      const files = res.data.files;
-      if (files.length) {
-        console.log('Files:');
-        files.map((file) => {
-          console.log(`${file.name} (${file.id})`);
-        });
-      } else {
-        console.log('No files found.');
-      }
-    });
-  }
 
 document.addEventListener('DOMContentLoaded', function () {
-    
-    const textarea = document.getElementById('markdown');
-    const preview = document.getElementById('preview');
-    const toggleBtn = document.getElementById('toggle-btn');
-    const sidebar = document.getElementById('sidebar');
-    const workspace = document.getElementById('workspace');
 
-    toggleBtn.addEventListener('click', () => {
-        sidebar.classList.toggle('collapsed');
-        if (sidebar.classList.contains('collapsed')) {
-            workspace.style.marginLeft = "50px";
-        } 
-        
-        else {
-            workspace.style.marginLeft = "250px";
-        }
-    });
+  const textarea = document.getElementById('markdown');
+  const preview = document.getElementById('preview');
+  const toggleBtn = document.getElementById('toggle-btn');
+  const sidebar = document.getElementById('sidebar');
+  const workspace = document.getElementById('workspace');
 
-    function updatePreview() {
-        const markdownText = textarea.value;
-        preview.innerHTML = marked.parse(markdownText);
+  toggleBtn.addEventListener('click', () => {
+    sidebar.classList.toggle('collapsed');
+    if (sidebar.classList.contains('collapsed')) {
+      workspace.style.marginLeft = "50px";
     }
 
-    function loadNote() {
-        console.warn("Load and save are not yet implemented");
+    else {
+      workspace.style.marginLeft = "250px";
     }
+  });
 
-    textarea.addEventListener('input', updatePreview);
+  function updatePreview() {
+    const markdownText = textarea.value;
+    preview.innerHTML = marked.parse(markdownText);
+  }
 
-    // Load the saved note on page load
-    loadNote();
+  function loadNote() {
+    console.warn("Load and save are not yet implemented");
+  }
+
+  textarea.addEventListener('input', updatePreview);
+
+  // Load the saved note on page load
+  loadNote();
 });
+
+*/
+
+/* exported gapiLoaded */
+/* exported gisLoaded */
+/* exported handleAuthClick */
+/* exported handleSignoutClick */
+
+// TODO(developer): Set to client ID and API key from the Developer Console
+const CLIENT_ID = import.meta.env.VITE_CLIENT_ID;
+const API_KEY = '<YOUR_API_KEY>';
+
+// Discovery doc URL for APIs used by the quickstart
+const DISCOVERY_DOC = 'https://www.googleapis.com/discovery/v1/apis/drive/v3/rest';
+
+// Authorization scopes required by the API; multiple scopes can be
+// included, separated by spaces.
+const SCOPES = 'https://www.googleapis.com/auth/drive.metadata.readonly';
+
+let tokenClient;
+let gapiInited = false;
+let gisInited = false;
+
+document.getElementById('authorize_button').style.visibility = 'hidden';
+document.getElementById('signout_button').style.visibility = 'hidden';
+
+/**
+ * Callback after api.js is loaded.
+ */
+function gapiLoaded() {
+  gapi.load('client', initializeGapiClient);
+}
+
+/**
+ * Callback after the API client is loaded. Loads the
+ * discovery doc to initialize the API.
+ */
+async function initializeGapiClient() {
+  await gapi.client.init({
+    apiKey: API_KEY,
+    discoveryDocs: [DISCOVERY_DOC],
+  });
+  gapiInited = true;
+  maybeEnableButtons();
+}
+
+/**
+ * Callback after Google Identity Services are loaded.
+ */
+function gisLoaded() {
+  tokenClient = google.accounts.oauth2.initTokenClient({
+    client_id: CLIENT_ID,
+    scope: SCOPES,
+    callback: '', // defined later
+  });
+  gisInited = true;
+  maybeEnableButtons();
+}
+
+/**
+ * Enables user interaction after all libraries are loaded.
+ */
+function maybeEnableButtons() {
+  if (gapiInited && gisInited) {
+    document.getElementById('authorize_button').style.visibility = 'visible';
+  }
+}
+
+/**
+ *  Sign in the user upon button click.
+ */
+function handleAuthClick() {
+  tokenClient.callback = async (resp) => {
+    if (resp.error !== undefined) {
+      throw (resp);
+    }
+    document.getElementById('signout_button').style.visibility = 'visible';
+    document.getElementById('authorize_button').innerText = 'Refresh';
+    await listFiles();
+  };
+
+  if (gapi.client.getToken() === null) {
+    // Prompt the user to select a Google Account and ask for consent to share their data
+    // when establishing a new session.
+    tokenClient.requestAccessToken({ prompt: 'consent' });
+  } else {
+    // Skip display of account chooser and consent dialog for an existing session.
+    tokenClient.requestAccessToken({ prompt: '' });
+  }
+}
+
+/**
+ *  Sign out the user upon button click.
+ */
+function handleSignoutClick() {
+  const token = gapi.client.getToken();
+  if (token !== null) {
+    google.accounts.oauth2.revoke(token.access_token);
+    gapi.client.setToken('');
+    document.getElementById('content').innerText = '';
+    document.getElementById('authorize_button').innerText = 'Authorize';
+    document.getElementById('signout_button').style.visibility = 'hidden';
+  }
+}
+
+/**
+ * Print metadata for first 10 files.
+ */
+async function listFiles() {
+  let response;
+  try {
+    response = await gapi.client.drive.files.list({
+      'pageSize': 10,
+      'fields': 'files(id, name)',
+    });
+  } catch (err) {
+    document.getElementById('content').innerText = err.message;
+    return;
+  }
+  const files = response.result.files;
+  if (!files || files.length == 0) {
+    document.getElementById('content').innerText = 'No files found.';
+    return;
+  }
+  // Flatten to string to display
+  const output = files.reduce(
+    (str, file) => `${str}${file.name} (${file.id})\n`,
+    'Files:\n');
+  document.getElementById('content').innerText = output;
+}
